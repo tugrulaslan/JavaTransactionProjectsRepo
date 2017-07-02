a9fcf587a60df353fcec1007d30e370fa3db2d4c
= <tugrulaslan@gmail.com>
2017-07-02 23:10:05 +0200

    removed the ddl sql file from the project

--- SpringTransactionProject/src/main/resources/create-drop-schema-query.sql
+++
@@ -1,7 +0,0 @@
-DROP SCHEMA IF EXISTS `springtx`;
-CREATE SCHEMA `springtx` ;
-CREATE TABLE `springtx`.`employee` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `department` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
